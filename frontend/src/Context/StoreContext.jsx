223ec7f1dce1f25f979406264172590ea19e895e
IMPOSTER03 <ykavish03@gmail.com>
2024-07-02 13:34:48 +0530

    Responsiveness changes in frontend

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ import { books_data } from "../assets/assets";
 export const StoreContext=createContext(null);
 
 const StoreContextProvider=(props)=>{
-    const URL="http://localhost:4000";
+    const URL="http://10.168.10.37:4000";
 
     const [token,setToken]=useState('');
     const [subCategory,setSubCategory]=useState('');
